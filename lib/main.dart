a1eebc7ba34ae19ff481f86c0f5dd5e4918530b8
Aliflkpd <andialifnurman@gmail.com>
2023-06-11 16:27:49 +0800

    connection to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyDL_V_vT2iuxtO8GD4cZm2Cq3bGnNvhLcg',
+          appId: '1:402167014063:web:3986150202bb718c3faa5a',
+          messagingSenderId: '402167014063',
+          projectId: 'instagram-clone-42b89',
+          storageBucket: 'instagram-clone-42b89.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 

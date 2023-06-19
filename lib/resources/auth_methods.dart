76d30bb6c18d135ca6afe5b7493c903370d3b05a
Aliflkpd <andialifnurman@gmail.com>
2023-06-19 08:58:04 +0800

    removed unused imports

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:instagram_flutter/models/user.dart' as model;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram_flutter/resources/storage_method.dart';
